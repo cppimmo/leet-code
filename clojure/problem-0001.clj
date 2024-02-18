2acf21ec81f9ef0d0c6977dff67df8af05fa3441
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2024-02-17 23:50:27 -0600

    Solve problem 1 in Clojure

--- clojure/problem-0001.clj
+++ clojure/problem-0001.clj
@@ -1,18 +1,24 @@
 (ns problem-0001
   (:use [clojure.test :only [is]]))
 
-(declare two-sum)
+(declare two-sum) ; Forward declare
 
-(do
+(do ; 3 test cases
   (is (= (two-sum [2 7 11 15] 9) [0 1]))
   (is (= (two-sum [3 2 4] 6) [1 2]))
   (is (= (two-sum [3 3] 6) [0 1])))
 
 (defn two-sum [nums target]
-  (loop [idx 0
-         elems (hash-map)]
-    (let [curr-elem (nth nums idx)
-          complement (- target curr-elem)]
-      (if (contains? elems complement)
-        (get elems complement)
-        (recur (inc idx) (assoc elems curr-elem idx))))))
+  (loop [idx     0
+         indexes (->> nums     ; Indexes map contains k: num, v: index
+                      (map-indexed (fn [& args] (vec (reverse args))))
+                      (reduce merge {}))]
+    (if (>= idx (count nums))
+      nil ; No summation of two elements equals target
+      (let [elem (nth nums idx), complement (- target elem)]
+        (if (and (contains? indexes complement)
+                 ; Ensure the same element is not considered
+                 ; lazy eval ensures calling get here isn't a no no
+                 (not= idx (get indexes complement)))
+          [idx (get indexes complement)] ; Return result
+          (recur (inc idx) indexes)))))) ; Continue search
