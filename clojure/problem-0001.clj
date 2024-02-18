cd4e1ba1c0ee7875f5749e380ad7ba1885d0201d
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2024-02-18 15:41:36 -0600

    Fix test cases in Clojure problem 1

--- clojure/problem-0001.clj
+++ clojure/problem-0001.clj
@@ -1,12 +1,16 @@
 (ns problem-0001
-  (:use [clojure.test :only [is]]))
+  (:use [clojure.test :only [deftest is testing]]))
 
 (declare two-sum) ; Forward declare
 
-(do ; 3 test cases
-  (is (= (two-sum [2 7 11 15] 9) [0 1]))
-  (is (= (two-sum [3 2 4] 6) [1 2]))
-  (is (= (two-sum [3 3] 6) [0 1])))
+(defn set= [& args] ; Equate collections no matter their element ordering
+  (apply = (map set args)))
+
+(deftest two-sum-test
+  (testing "Three test cases; ordering doesn't matter."
+    (is (set= (two-sum [2 7 11 15] 9) [0 1]))
+    (is (set= (two-sum [3 2 4] 6) [1 2]))
+    (is (set= (two-sum [3 3] 6) [0 1]))))
 
 (defn two-sum [nums target]
   (loop [idx     0
