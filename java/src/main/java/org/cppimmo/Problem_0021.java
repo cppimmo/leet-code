8ec95a7a2029fbda0580004f6482c5ffdfffe79a
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2024-08-19 18:18:28 -0500

    Add other Java solution for problem 21

--- java/src/main/java/org/cppimmo/Problem_0021.java
+++ java/src/main/java/org/cppimmo/Problem_0021.java
@@ -9,7 +9,8 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+	// Brute-force implementation
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
         List<Integer> numbers = new ArrayList<>(100);
         // Add the numbers from the list nodes to the array
         iterate(list1, numbers);
@@ -27,10 +28,36 @@ class Solution {
     }
 
     private static void iterate(final ListNode head, List<Integer> numbers) {
+		// Add the numbers from the linked-list to the list, numbers.
         ListNode current = head;
         while (current != null) {
             numbers.add(current.val);
             current = current.next;
         }
+    }
+	// In-place implementation
+	public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        // Dummy head node provides a means to attach the rest of the nodes
+        ListNode head = new ListNode(), current = head;
+        while (list1 != null && list2 != null) {
+            ListNode node;
+            if (list1.val < list2.val) {
+                node = list1;
+                list1 = list1.next;
+            } else {
+                node = list2;
+                list2 = list2.next;
+            }
+            current.next = node; // Set the next node which was chosen
+            current = current.next;
+        }
+        // If the lists weren't the same length, advance the list that still has elements.
+        if (list1 != null) {
+            current.next = list1;
+        }
+        if (list2 != null) {
+            current.next = list2;
+        }
+        return head.next; // Advance past the dummy node (could be null)
     }
 }
