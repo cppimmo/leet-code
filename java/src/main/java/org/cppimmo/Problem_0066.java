349c1f806aee54a8e05d470ce53972fe96a30063
Brian Hoffpauir <bhoffpauir0424@gmail.com>
2024-08-21 17:30:48 -0500

    Add another Java solution for problem 66

--- java/src/main/java/org/cppimmo/Problem_0066.java
+++ java/src/main/java/org/cppimmo/Problem_0066.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 
 class Solution {
-    public int[] plusOne(int[] digits) {
+    public int[] plusOne1(int[] digits) {
         return intToDigits(digitsToInt(digits).add(BigInteger.ONE));
     }
 
@@ -20,5 +20,27 @@ class Solution {
             digits[i] = (char)(numStr.charAt(i) - '0');
         }
         return digits;
+    }
+
+	public int[] plusOne2(int[] digits) {
+        int carry = 1;
+        // Begin iterating at the least significant digit
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += carry;
+                carry = 0;
+                break;
+            } else
+                digits[i] = 0;
+        }
+
+        if (carry > 0) {
+            int[] newDigits = new int[digits.length + 1];
+            System.arraycopy(digits, 0, newDigits, 1, newDigits.length - 1);
+            newDigits[0] = carry;
+            return newDigits;
+        }
+        else
+            return digits;
     }
 }
